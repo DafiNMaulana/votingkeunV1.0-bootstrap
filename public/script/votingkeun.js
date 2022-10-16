8fcb689acd842ed3ad9504cdfe41be069e08372e
DafiNMaulana <unoobcombro@gmail.com>
2022-10-16 09:49:06 +0700

    js add:conditional, bilikSuara1 fixed: accordion panel stay open

--- public/script/votingkeun.js
+++ public/script/votingkeun.js
@@ -1,19 +1,28 @@
 // *** Show visi misi ***
-// **angka na ubah jadi id DB**
 // **kuduna teu perlu nien 2 function kieu cuman duka mun tiap angka na digenti ku ID DB**
-// **Mun teu jalan mah hapus/coment we nu penting mah ketika klik tombol plus muncul detail na**
+// **angka na ubah jadi angka nu berasal ti id DB**
+// **Mun teu jalan/mbung ambil pusing mah hapus/coment we 
+// nu penting mah ketika klik tombol plus muncul detail na**
 // **asal eta jalan cukup, bisi hayang aya perubahan tina UI/UX bisa diskusi heula**
 
 let iconPlus = document.getElementById('iconPlus');
 let iconPlus2 = document.getElementById('iconPlus2');
+let btnVimPro = document.getElementById('btnVimPro');
+let btnVimPro2 = document.getElementById('btnVimPro2');
 
 function btnVisiMisi() {
     if (iconPlus.classList.contains("fa-plus")) {
         iconPlus.classList.remove("fa-plus");
         iconPlus.classList.add("fa-minus");
+
+        btnVimPro.classList.remove('btn-outline-primary');
+        btnVimPro.classList.add('btn-primary');
       } else if (iconPlus.classList.contains("fa-minus")) {
         iconPlus.classList.remove("fa-minus");
         iconPlus.classList.add("fa-plus")
+
+        btnVimPro.classList.remove('btn-primary');
+        btnVimPro.classList.add('btn-outline-primary');
     };
 }
 
@@ -21,8 +30,14 @@ function btnVisiMisi2() {
     if (iconPlus2.classList.contains("fa-plus")) {
         iconPlus2.classList.remove("fa-plus");
         iconPlus2.classList.add("fa-minus");
+
+        btnVimPro2.classList.remove('btn-outline-primary');
+        btnVimPro2.classList.add('btn-primary');
       } else if (iconPlus2.classList.contains("fa-minus")) {
         iconPlus2.classList.remove("fa-minus");
         iconPlus2.classList.add("fa-plus")
+
+        btnVimPro2.classList.remove('btn-primary');
+        btnVimPro2.classList.add('btn-outline-primary');
     };
 }
